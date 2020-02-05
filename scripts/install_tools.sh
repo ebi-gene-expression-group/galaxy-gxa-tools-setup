9d73c54cab71d810ca35aa300b23b70858e61689
Pablo Moreno <pcm32@users.noreply.github.com>
2020-02-05 17:28:02 +0000

    Make dependency installation optional
    Mostly for kubernetes deployments

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,9 +1,17 @@
 GALAXY_INSTANCE=$1
 API_KEY=$2
+NO_DEPS=${3:-false}
+
+INSTALL_DEPS="--install_resolver_dependencies"
+
+if [ "$NO_DEPS" = "no-deps" ]; then
+  INSTALL_DEPS=""
+  echo "Not using --install_resolver_dependencies as instructed"
+fi
 
 for lock_file in $(ls $DIR_WITH_LOCK_FILES | grep .lock); do
   name=$( echo $lock_file | sed 's/.yaml.lock//' )
-  shed-tools install --install_resolver_dependencies \
+  shed-tools install $INSTALL_DEPS \
     --toolsfile $lock_file \
     --galaxy $GALAXY_INSTANCE \
     --api_key $API_KEY 2>&1 | tee -a $name.log
