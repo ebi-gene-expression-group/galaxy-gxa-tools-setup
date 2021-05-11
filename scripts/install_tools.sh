bd577f9487f127176f0d16a967e990612893f026
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-05-11 16:53:07 +0100

    Fix flag for not installing conda packages

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -5,8 +5,8 @@ NO_DEPS=${3:-false}
 INSTALL_DEPS="--install_resolver_dependencies"
 
 if [ "$NO_DEPS" = "no-deps" ]; then
-  INSTALL_DEPS=""
-  echo "Not using --install_resolver_dependencies as instructed"
+  INSTALL_DEPS="--skip_install_resolver_dependencies"
+  echo "Not using --install_resolver_dependencies and using --skip_install_resolver_dependencies"
 fi
 
 for lock_file in $(ls $DIR_WITH_LOCK_FILES | grep .lock); do
